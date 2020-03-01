067f31948ef842345bdbdd9bb1af26e17003bc0d
Kang Jie Tan <>
2020-02-29 19:32:38 -0800

    GET request to Yelp API

--- api/searchYelp.js
+++ api/searchYelp.js
@@ -1,8 +1,29 @@
-import YELP_API_KEY from '../config/config.js';
 import axios from 'axios';
+import YELP_API_KEY from '../config/config.js';
 
-const searchYelp = () => {
+const searchYelp = (term, location) => {
+  const cors = 'https://cors-anywhere.herokuapp.com';
+  const url = 'https://api.yelp.com/v3/businesses/search';
 
+  const options = {
+    method: 'GET',
+    headers: { 'Authorization': YELP_API_KEY },
+    params: {
+      term,
+      location,
+    },
+    url: `${cors}/${url}`,
+  };
+
+  return new Promise((resolve, reject) => {
+    axios(options)
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 };
 
 export default searchYelp;
--- server/index.js
+++ server/index.js
@@ -3,10 +3,15 @@ const express = require('express');
 // Import the path module from node to create absolute file paths for express static
 const path = require('path');
 
+// const cors = require('cors');
+
 // Instantiate the express server
 const app = express();
 // Set a constant for the port that our express server will listen on
 const PORT = 3000;
+
+// Cors
+// app.use(cors());
 
 // Serve static files. Any requests for specific files will be served if they exist in the provided folder
 app.use(express.static(path.join(__dirname, '../client/dist')));
