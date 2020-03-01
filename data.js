778622b8688de41c7cb450a96581d32eb281c26e
Kang Jie Tan <>
2020-03-01 00:57:45 -0800

    Dish names and image urls

---
+++ data.js
@@ -0,0 +1,249 @@
+module.exports = {
+  dishes: [
+    {
+      dish_name: 'Bibimbap',
+      image_url: 'https://www.budgetbytes.com/wp-content/uploads/2017/01/Bibimbap-above.jpg',
+    },
+    {
+      dish_name: 'Burrito',
+      image_url: 'https://www.onceuponachef.com/images/2018/03/Breakfast-Burritos-1024x766.jpg',
+    },
+    {
+      dish_name: 'Cheeseburger',
+      image_url: 'https://i2.wp.com/www.foodrepublic.com/wp-content/uploads/2012/03/033_FR11785.jpg?resize=700%2C%20466&ssl=1',
+    },
+    {
+      dish_name: 'Kimchi Fried Rice',
+      image_url: 'https://pinchofyum.com/wp-content/uploads/Kimchi-Fried-Rice-Recipe.jpg',
+    },
+    {
+      dish_name: 'Nasi Goreng',
+      image_url: 'https://www.seriouseats.com/2019/06/20190604-nasi-goreng-fried-rice-vicky-wasik-7.jpg',
+    },
+    {
+      dish_name: 'Taco',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/7/73/001_Tacos_de_carnitas%2C_carne_asada_y_al_pastor.jpg',
+    },
+    {
+      dish_name: 'Hot dog',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Hotdog_-_Evan_Swigart.jpg/1200px-Hotdog_-_Evan_Swigart.jpg',
+    },
+    {
+      dish_name: 'Corn dog',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/c/c1/CornDog.jpg',
+    },
+    {
+      dish_name: 'Korean corn dog',
+      image_url: 'https://kirbiecravings.com/wp-content/uploads/2019/07/korean-hot-dogs-6.jpg',
+    },
+    {
+      dish_name: 'Chow Mein',
+      image_url: 'https://s23209.pcdn.co/wp-content/uploads/2019/05/Easy-Chow-MeinIMG_9789-360x360.jpg',
+    },
+    {
+      dish_name: 'Gumbo',
+      image_url: 'https://spicysouthernkitchen.com/wp-content/uploads/seafood-gumbo-12-500x375.jpg',
+    },
+    {
+      dish_name: 'Steak',
+      image_url: 'https://www.jessicagavin.com/wp-content/uploads/2018/06/how-to-reverse-sear-a-steak-11.jpg',
+    },
+    {
+      dish_name: 'Fried Rice',
+      image_url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-fried-rice-020-1543875572.jpg',
+    },
+    {
+      dish_name: 'Fried chicken',
+      image_url: 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/11/2/0/DV1510H_fried-chicken-recipe-10_s4x3.jpg.rend.hgtvcom.406.305.suffix/1568222255998.jpeg',
+    },
+    {
+      dish_name: 'Pizza',
+      image_url: 'https://www.simplyrecipes.com/wp-content/uploads/2019/09/easy-pepperoni-pizza-lead-4.jpg',
+    },
+    {
+      dish_name: 'Grilled Cheese',
+      image_url: 'https://assets.bonappetit.com/photos/57acf62a53e63daf11a4dbee/master/pass/best-ever-grilled-cheese.jpg',
+    },
+    {
+      dish_name: 'Sashimi',
+      image_url: 'https://www.japanesecooking101.com/wp-content/uploads/2019/08/DSC00528b.jpg',
+    },
+    {
+      dish_name: 'Sushi',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Sushi_platter.jpg/1200px-Sushi_platter.jpg',
+    },
+    {
+      dish_name: 'Pork Chops',
+      image_url: 'https://rasamalaysia.com/wp-content/uploads/2018/10/garlic-butter-pork-chops.jpg',
+    },
+    {
+      dish_name: 'Tofu Stew',
+      image_url: 'https://mykoreankitchen.com/wp-content/uploads/2006/11/1.-Sundubu-Jjigae-Korean-spicy-soft-tofu-stew-500x500.jpg',
+    },
+    {
+      dish_name: 'Ramen',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Shoyu_Ramen.jpg/1200px-Shoyu_Ramen.jpg',
+    },
+    {
+      dish_name: 'Ice Cream',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/3/31/Ice_Cream_dessert_02.jpg',
+    },
+    {
+      dish_name: 'Crepe',
+      image_url: 'https://www.thespruceeats.com/thmb/uP73Cn6wqnG39ivYZa7GRXvyqX0=/1000x1000/smart/filters:no_upscale()/basic-crepe-recipe-sweet-or-savory-995317-hero-5beb1445c9e77c0026f2bbb5.jpg',
+    },
+    {
+      dish_name: 'Fruit Salad',
+      image_url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/pasta-salad-horizontal-jpg-1522265695.jpg',
+    },
+    {
+      dish_name: 'Lobster',
+      image_url: 'https://www.simplyrecipes.com/wp-content/uploads/2010/08/how-to-boil-eat-lobster-horiz-a-1800.jpg',
+    },
+    {
+      dish_name: 'Sliders',
+      image_url: 'http://res.cloudinary.com/hksqkdlah/image/upload/v1535323934/42415-sfs-sliders-reshoot-32.jpg',
+    },
+    {
+      dish_name: 'Apple Pie',
+      image_url: 'https://images-gmi-pmc.edge-generalmills.com/75593ed5-420b-4782-8eae-56bdfbc2586b.jpg',
+    },
+    {
+      dish_name: 'Adobo',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Chicken_adobo.jpg/1200px-Chicken_adobo.jpg',
+    },
+    {
+      dish_name: 'Dim Sum',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/1/13/Dim_sum.jpg',
+    },
+    {
+      dish_name: 'Fries',
+      image_url: 'https://cms.splendidtable.org/sites/default/files/styles/w2000/public/french-fries.jpg?itok=FS-YwUYH',
+    },
+    {
+      dish_name: 'Clam Chowder',
+      image_url: 'https://s23209.pcdn.co/wp-content/uploads/2019/10/Easy-Clam-ChowderIMG_1064.jpg',
+    },
+    {
+      dish_name: 'Acai Bowl',
+      image_url: 'https://www.cookingclassy.com/wp-content/uploads/2019/05/acai-bowl-23.jpg',
+    },
+    {
+      dish_name: 'Baked Potato',
+      image_url: 'https://www.foodnetwork.com/content/dam/images/food/video/0/01/016/0169/0169272.jpg',
+    },
+    {
+      dish_name: 'Churros',
+      image_url: 'https://www.cookingclassy.com/wp-content/uploads/2018/08/churros-55.jpg',
+    },
+    {
+      dish_name: 'Enchiladas',
+      image_url: 'http://www.gimmesomeoven.com/wp-content/uploads/2014/10/The-Best-Chicken-Enchiladas-Recipe-1-1.jpg',
+    },
+    {
+      dish_name: 'Mashed Potato',
+      image_url: 'https://cafedelites.com/wp-content/uploads/2018/11/Creamy-Mash-Potatoes-IMAGE-4.jpg',
+    },
+    {
+      dish_name: 'Coleslaw',
+      image_url: 'https://www.fifteenspatulas.com/wp-content/uploads/2018/07/Cole-Slaw-Fifteen-Spatulas-1-640x959.jpg',
+    },
+    {
+      dish_name: 'Mac N Cheese',
+      image_url: 'https://www.thechunkychef.com/wp-content/uploads/2018/02/Ultimate-Creamy-Baked-Mac-and-Cheese-bowl-680x845.jpg,',
+    },
+    {
+      dish_name: 'Quesadilla',
+      image_url: 'https://www.cookingclassy.com/wp-content/uploads/2019/02/quesadillas-2.jpg',
+    },
+    {
+      dish_name: 'Curry Fishballs',
+      image_url: 'https://thewoksoflife.com/wp-content/uploads/2017/02/curry-fish-balls-13-1-500x375.jpg',
+    },
+    {
+      dish_name: 'King Crab',
+      image_url: 'https://thestayathomechef.com/wp-content/uploads/2019/02/How-to-Cook-Crab-Legs-1-500x375.jpg',
+    },
+    {
+      dish_name: 'Takoyaki',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Takoyaki.jpg/1200px-Takoyaki.jpg',
+    },
+    {
+      dish_name: 'Wine',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Red_and_white_wine_12-2015.jpg',
+    },
+    {
+      dish_name: 'Smoothies',
+      image_url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-how-to-make-a-smoothie-horizontal-1542310071.png?crop=0.803xw:0.923xh;0.116xw,0.00510xh&resize=480:*',
+    },
+    {
+      dish_name: 'Milkshakes',
+      image_url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-how-to-make-a-smoothie-horizontal-1542310071.png?crop=0.803xw:0.923xh;0.116xw,0.00510xh&resize=480:*',
+    },
+    {
+      dish_name: 'Fish N Chips',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Fish_and_chips_blackpool.jpg',
+    },
+    {
+      dish_name: 'Boba',
+      image_url: 'https://www.ohhowcivilized.com/wp-content/uploads/2019/05/0519-bubble-tea-14-735x1103.jpg',
+    },
+    {
+      dish_name: 'Horchata',
+      image_url: 'https://tastesbetterfromscratch.com/wp-content/uploads/2016/04/Horchata-2.jpg',
+    },
+    {
+      dish_name: 'Roast Beef',
+      image_url: 'https://hips.hearstapps.com/hmg-prod/images/delish-roast-beef-horizontal-1540505165.jpg?crop=1.00xw%3A0.846xh%3B0%2C0&resize=480%3A270',
+    },
+    {
+      dish_name: 'Spaghetti',
+      image_url: 'https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg',
+    },
+    {
+      dish_name: 'Risotto',
+      image_url: 'https://www.thespruceeats.com/thmb/Bl0-QjmXZYKNdTqLoL0qbxp44IY=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/risotto-recipe-for-beginners-996008-13_preview-5b0438a4a9d4f9003656fdee.jpeg',
+    },
+    {
+      dish_name: 'Chicken Alfredo',
+      image_url: 'https://hips.hearstapps.com/delish/assets/17/24/1497458683-delish-one-pot-chicken-alfredo-1-1024.jpg',
+    },
+    {
+      dish_name: 'Chicken Carbonara',
+      image_url: 'https://hips.hearstapps.com/del.h-cdn.co/assets/16/32/1470681018-delish-chicken-carbonara-pin-3.jpg',
+    },
+    {
+      dish_name: 'Corn on the Cob',
+      image_url: 'https://hips.hearstapps.com/hmg-prod/images/boiled-corn-horizontal-1539104513.png',
+    },
+    {
+      dish_name: 'Pasta',
+      image_url: 'https://www.thespruceeats.com/thmb/hJtP5QhBfZayjUD7RCyVbydJHtk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-885397466-5c0cc0634cedfd00012758a7.jpg',
+    },
+    {
+      dish_name: 'Banh Mi',
+      image_url: 'https://rasamalaysia.com/wp-content/uploads/2012/06/lemongrass-pork-banhmi-thum-1.jpg',
+    },
+    {
+      dish_name: 'Pho',
+      image_url: 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/11/30/0/FNK_Instant-Pot-Beef-Pho-H_s4x3.jpg.rend.hgtvcom.826.620.suffix/1548176890147.jpeg',
+    },
+    {
+      dish_name: 'Udon',
+      image_url: 'https://www.justonecookbook.com/wp-content/uploads/2015/11/Niku-Udon.jpg',
+    },
+  ]
+}
+
+// {
+//   dish_name: '',
+//   image_url: '',
+// },
+// {
+//   dish_name: '',
+//   image_url: '',
+// },
+// {
+//   dish_name: '',
+//   image_url: '',
+// },
