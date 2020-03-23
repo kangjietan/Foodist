c0b2c8dcc8d72edf3987db37efab4fba11a35600
kangjietan <>
2020-03-23 00:42:24 -0700

    Switch back to dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-// const webpack = require('webpack');
+const webpack = require('webpack');
 
 const path = require('path');
 
-// const dotenv = require('dotenv').config({ path: `${__dirname}/.env` });
-const Dotenv = require('dotenv-webpack');
+const dotenv = require('dotenv').config({ path: `${__dirname}/.env` });
+// const Dotenv = require('dotenv-webpack');
 
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
@@ -26,13 +26,13 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new Dotenv(),
-  ],
   // plugins: [
-  //   new webpack.DefinePlugin({
-  //     'process.env.YELP_API_KEY': JSON.stringify(dotenv.parsed.YELP_API_KEY),
-  //     'process.env.GOOGLE_API_KEY': JSON.stringify(dotenv.parsed.GOOGLE_API_KEY),
-  //   }),
+  //   new Dotenv(),
   // ],
+  plugins: [
+    new webpack.DefinePlugin({
+      // 'process.env.YELP_API_KEY': JSON.stringify(dotenv.parsed.YELP_API_KEY),
+      'process.env.GOOGLE_API_KEY': JSON.stringify(dotenv.parsed.GOOGLE_API_KEY),
+    }),
+  ],
 };
