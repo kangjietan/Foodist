6a5f61ceabc391eaab552f9cf050710702209cb1
kangjietan <kangjietan@gmail.com>
2020-07-16 17:28:44 -0700

    Formatting

--- api/searchYelp.js
+++ api/searchYelp.js
@@ -1,16 +1,19 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const axios = require('axios');
+const axios = require("axios");
 
 const { YELP_API_KEY } = process.env;
 
 const searchYelp = (params) => {
-  const cors = 'https://cors-anywhere.herokuapp.com';
-  const url = 'https://api.yelp.com/v3/businesses/search';
+  const cors = "https://cors-anywhere.herokuapp.com";
+  const url = "https://api.yelp.com/v3/businesses/search";
 
   const options = {
-    method: 'GET',
-    headers: { Authorization: YELP_API_KEY, 'X-Requested-With': 'XMLHttpRequest' },
+    method: "GET",
+    headers: {
+      Authorization: YELP_API_KEY,
+      "X-Requested-With": "XMLHttpRequest",
+    },
     params,
     url: `${cors}/${url}`,
   };
