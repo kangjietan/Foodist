cd1072b570109b188bfbcb3e0f036ed418a6c604
kangjietan <kangjietan@gmail.com>
2021-02-11 14:51:55 -0800

    Remove cors proxy

--- api/searchYelp.js
+++ api/searchYelp.js
@@ -5,7 +5,6 @@ const axios = require("axios");
 const { YELP_API_KEY } = process.env;
 
 const searchYelp = (params) => {
-  const cors = "https://cors-anywhere.herokuapp.com";
   const url = "https://api.yelp.com/v3/businesses/search";
 
   const options = {
@@ -15,7 +14,7 @@ const searchYelp = (params) => {
       "X-Requested-With": "XMLHttpRequest",
     },
     params,
-    url: `${cors}/${url}`,
+    url,
   };
 
   return new Promise((resolve, reject) => {
